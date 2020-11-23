c4636d4c8470be28ad228ea242247fa07583834f
bolicoding <bolicoding@gmail.com>
2020-11-23 15:38:26 -0700

    added navlinks

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,13 +2,54 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <div>
       <div className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            Fast Garage <i className="fab fa-typo3"></i>
+            Fast Garage <i className="fab fa-typo3" />
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-manu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
